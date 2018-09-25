cbda0f6b992a566474944185efc8bad842dc0c2e
Sean Wu <panda@ms5.url.com.tw>
2018-09-25 17:14:26 +0800

    Update 複雜度測試 .py

--- 複雜度測試 .py
+++ 複雜度測試 .py
@@ -1,5 +1,4 @@
-import hashlib
-
+import hashlib, datetime
 
 class seancoin:
     @staticmethod
@@ -13,24 +12,26 @@ class seancoin:
 
     def proof_of_work(self, message,i=1):
         nonce = 0
-        
+        s = datetime.datetime.now()
         while True:
             nonce += 1
             data = f'{message}{nonce}'
             if self.md5(data)[:i] == '0'*i:
                 if self.sha256(data)[:i] == '0'*i:
-                    return nonce
+                    e = datetime.datetime.now()
+                    return nonce,(e-s).total_seconds()
 
     def drawpof(self,message):
         i=1
         pof=[]
         while i<=3:
-            pof.append([i,self.proof_of_work(message,i)])
+            t,n = self.proof_of_work(message,i)
+            pof.append([i,t,n])
             i+=1
 
         for r in pof:
             data = f'{message}{r[1]}'
-            print(f'{r[0]}\n{r[1]}\n{seancoin.md5(data)}\n{seancoin.sha256(data)}')
+            print(f'複雜度：{r[0]}\n時間(s):{r[2]}\nNonce:{r[1]}\n{seancoin.md5(data)}\n{seancoin.sha256(data)}')
 
             
 a = seancoin()
@@ -38,18 +39,23 @@ msg = '{sean123}'
 a.drawpof(msg)
 
 
+"""
 
-""" result
-1
-10
+複雜度：1
+時間(s):0.0
+Nonce:10
 06735564c666da0825ef2eb983bfe025
 00777136201d684d100a574a022005b1be7d4cd4c40b8e8d6361515658ebec0f
-2
-51126
+複雜度：2
+時間(s):0.124797
+Nonce:51126
 00602fe18610e8680a94922ef6ec248a
 001b3c94adc50fc5ce484434b9f061ea198ab8b027f810a3f00e16eec1c3cad7
-3
-22172209
+複雜度：3
+時間(s):28.944258
+Nonce:22172209
 00021f964276e07fd760333f48494fc7
 00019158ccbf25c2edd53da49d31bbb1230f343bf10f5f36eca311710ceca5a2
+
 """
+
